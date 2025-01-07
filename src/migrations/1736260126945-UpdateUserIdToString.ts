f900b2d860959a6754e41545807d00dff17e9b52
emmydapson <iloriemmanuel00@gmail.com>
2025-01-07 18:06:15 +0100

    UPDATE

--- src/migrations/1736260126945-UpdateUserIdToString.ts
+++ src/migrations/1736260126945-UpdateUserIdToString.ts
@@ -7,7 +7,7 @@ export class UpdateUserIdToString1736260126945 implements MigrationInterface {
         // Alter the "user" table instead of creating it again
         await queryRunner.query(`
             ALTER TABLE "user"
-            ALTER COLUMN "id" TYPE character varying USING id::text;  -- Change UUID to string
+            ALTER COLUMN "id" SET DATA TYPE character varying USING "id"::text;  -- Change UUID to string
             ALTER COLUMN "role" SET DEFAULT 'user';  -- Set default role if not already set
         `);
 
