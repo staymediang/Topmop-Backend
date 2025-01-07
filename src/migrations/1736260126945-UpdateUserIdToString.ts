577aa7e9e94670af32eb2dc65d1634c1a286e5de
emmydapson <iloriemmanuel00@gmail.com>
2025-01-07 17:14:02 +0100

    fixed migrations

--- src/migrations/1736260126945-UpdateUserIdToString.ts
+++ src/migrations/1736260126945-UpdateUserIdToString.ts
@@ -9,39 +9,24 @@ export class UpdateUserIdToString1736260126945 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "bookings" ADD "userId" uuid NOT NULL`);
         await queryRunner.query(`ALTER TABLE "service" DROP COLUMN "createdById"`);
         await queryRunner.query(`ALTER TABLE "service" ADD "createdById" uuid`);
-        await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "frequency"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "frequency" character varying(50) NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "preferredDay"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "preferredDay" character varying(20)`);
-        await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "preferredTime"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "preferredTime" character varying(20)`);
-        await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "cleaningStartDate"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "cleaningStartDate" date`);
-        await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "accessInstructions"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "accessInstructions" text`);
-        await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "additionalInfo"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "additionalInfo" text`);
-        await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "referralSource"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "referralSource" character varying(100)`);
-        await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "firstName"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "firstName" character varying(100)`);
-        await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "lastName"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "lastName" character varying(100)`);
-        await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "contactNumber"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "contactNumber" character varying(15)`);
-        await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "email"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "email" character varying(100)`);
-        await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "address"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "address" text`);
-        await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "city"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "city" character varying(50)`);
-        await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "postalCode"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "postalCode" character varying(20)`);
-        await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "paymentType"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "paymentType" character varying(20) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "bookings" ALTER COLUMN "frequency" TYPE character varying(50)`);
+        await queryRunner.query(`ALTER TABLE "bookings" ALTER COLUMN "preferredDay" TYPE character varying(20)`);
+        await queryRunner.query(`ALTER TABLE "bookings" ALTER COLUMN "preferredTime" TYPE character varying(20)`);
+        await queryRunner.query(`ALTER TABLE "bookings" ALTER COLUMN "cleaningStartDate" TYPE date`);
+        await queryRunner.query(`ALTER TABLE "bookings" ALTER COLUMN "accessInstructions" TYPE text`);
+        await queryRunner.query(`ALTER TABLE "bookings" ALTER COLUMN "additionalInfo" TYPE text`);
+        await queryRunner.query(`ALTER TABLE "bookings" ALTER COLUMN "referralSource" TYPE character varying(100)`);
+        await queryRunner.query(`ALTER TABLE "bookings" ALTER COLUMN "firstName" TYPE character varying(100)`);
+        await queryRunner.query(`ALTER TABLE "bookings" ALTER COLUMN "lastName" TYPE character varying(100)`);
+        await queryRunner.query(`ALTER TABLE "bookings" ALTER COLUMN "contactNumber" TYPE character varying(15)`);
+        await queryRunner.query(`ALTER TABLE "bookings" ALTER COLUMN "email" TYPE character varying(100)`);
+        await queryRunner.query(`ALTER TABLE "bookings" ALTER COLUMN "address" TYPE text`);
+        await queryRunner.query(`ALTER TABLE "bookings" ALTER COLUMN "city" TYPE character varying(50)`);
+        await queryRunner.query(`ALTER TABLE "bookings" ALTER COLUMN "postalCode" TYPE character varying(20)`);
+        await queryRunner.query(`ALTER TABLE "bookings" ALTER COLUMN "paymentType" TYPE character varying(20)`);
         await queryRunner.query(`ALTER TABLE "bookings" ALTER COLUMN "amount" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "service" ADD CONSTRAINT "FK_87cf55c0575ef49843d7bf29397" FOREIGN KEY ("createdById") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD CONSTRAINT "FK_38a69a58a323647f2e75eb994de" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "service" ADD CONSTRAINT "FK_87cf55c0575ef49843d7bf29397" FOREIGN KEY ("createdById") REFERENCES "user"("id")`);
+        await queryRunner.query(`ALTER TABLE "bookings" ADD CONSTRAINT "FK_38a69a58a323647f2e75eb994de" FOREIGN KEY ("userId") REFERENCES "user"("id")`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
@@ -53,17 +38,17 @@ export class UpdateUserIdToString1736260126945 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "postalCode"`);
         await queryRunner.query(`ALTER TABLE "bookings" ADD "postalCode" character varying`);
         await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "city"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "city" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "bookings" ADD "city" character varying`);
         await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "address"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "address" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "bookings" ADD "address" character varying`);
         await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "email"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "email" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "bookings" ADD "email" character varying`);
         await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "contactNumber"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "contactNumber" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "bookings" ADD "contactNumber" character varying`);
         await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "lastName"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "lastName" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "bookings" ADD "lastName" character varying`);
         await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "firstName"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "firstName" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "bookings" ADD "firstName" character varying`);
         await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "referralSource"`);
         await queryRunner.query(`ALTER TABLE "bookings" ADD "referralSource" character varying`);
         await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "additionalInfo"`);
@@ -77,12 +62,11 @@ export class UpdateUserIdToString1736260126945 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "preferredDay"`);
         await queryRunner.query(`ALTER TABLE "bookings" ADD "preferredDay" character varying`);
         await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "frequency"`);
-        await queryRunner.query(`ALTER TABLE "bookings" ADD "frequency" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "bookings" ADD "frequency" character varying`);
         await queryRunner.query(`ALTER TABLE "service" DROP COLUMN "createdById"`);
-        await queryRunner.query(`ALTER TABLE "service" ADD "createdById" integer`);
+        await queryRunner.query(`ALTER TABLE "service" ADD "createdById" uuid`);
         await queryRunner.query(`ALTER TABLE "bookings" DROP COLUMN "userId"`);
         await queryRunner.query(`DROP TABLE "notifications"`);
         await queryRunner.query(`DROP TABLE "user"`);
     }
-
 }
