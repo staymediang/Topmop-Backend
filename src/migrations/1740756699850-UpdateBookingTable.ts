8e418b3e3508e767594bca3d72bd964bd75e409d
emmydapson <iloriemmanuel00@gmail.com>
2025-02-28 18:34:49 +0100

    database migration updated

--- src/migrations/1740756699850-UpdateBookingTable.ts
+++ src/migrations/1740756699850-UpdateBookingTable.ts
@@ -4,11 +4,7 @@ export class UpdateBookingTable1740756699850 implements MigrationInterface {
     name = 'UpdateBookingTable1740756699850'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(
-            `ALTER TABLE "service" ADD CONSTRAINT "FK_87cf55c0575ef49843d7bf29397" 
-            FOREIGN KEY ("createdById") REFERENCES "user"("id") 
-            ON DELETE NO ACTION ON UPDATE NO ACTION`
-        );
+        
 
         await queryRunner.query(
             `ALTER TABLE "bookings" ADD CONSTRAINT "FK_38a69a58a323647f2e75eb994de" 
@@ -22,8 +18,6 @@ export class UpdateBookingTable1740756699850 implements MigrationInterface {
             `ALTER TABLE "bookings" DROP CONSTRAINT "FK_38a69a58a323647f2e75eb994de"`
         );
 
-        await queryRunner.query(
-            `ALTER TABLE "service" DROP CONSTRAINT "FK_87cf55c0575ef49843d7bf29397"`
-        );
+        
     }
 }
