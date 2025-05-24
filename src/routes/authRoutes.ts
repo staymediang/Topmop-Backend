88626eb0a757c8cba61a1ddaaad79e7f574001e0
emmydapson <iloriemmanuel00@gmail.com>
2025-05-24 12:47:58 +0100

    updated auth routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { Router } from "express";
 import { signup, login,  } from "../controllers/authControllers";
+import { sendResetPasswordLink, resetPassword } from "../controllers/authControllers";
 
 const router = Router();
 
 router.post("/signup", signup );
 router.post("/login", login );
+router.post("/send-reset-link", sendResetPasswordLink);
+router.post("/reset-password", resetPassword);
 
 export default router;
